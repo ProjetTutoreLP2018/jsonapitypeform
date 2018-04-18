636799f61435c517abcc1f137099f3964e486990
ProjetTutoreLP2018 <37624544+ProjetTutoreLP2018@users.noreply.github.com>
2018-04-18 14:02:43 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,114 +13,38 @@ namespace ConsoleApp1
 
     public class Settings
     {
-
-
-        public bool is_public { get; set; }
-        public bool is_trial { get; set; }
-    }
-
-    public class Self
-    {
-        public string href { get; set; }
-    }
-
-    public class Theme
-    {
-        public string href { get; set; }
-    }
-
-    public class Links
-    {
-        public string display { get; set; }
-    }
-
-    public class Item
-    {
-        public string id { get; set; }
-        public string title { get; set; }
-        public DateTime last_updated_at { get; set; }
-        public Settings settings { get; set; }
-        public Self self { get; set; }
-        public Theme theme { get; set; }
-        public Links _links { get; set; }
-    }
-
-    public class RootObject
-    {
-        public int total_items { get; set; }
-        public int page_count { get; set; }
-        public List<Item> items { get; set; }
-    }
-
-
-
-
-
-
-
-
-    class Program
-    {
-        static string token = "C4yr7FzMfPjiXd6QRMt1ypPT1tUWMbYvk4UQgxdAnrmL";
-        static string id_form = "YuE8Lp";
-        async static Task req()
+        class Program
         {
+            static string token = "C4yr7FzMfPjiXd6QRMt1ypPT1tUWMbYvk4UQgxdAnrmL";
+            static string id_form = "YuE8Lp";
 
-            string url = "https://api.typeform.com/forms/" + id_form + "/responses";
-            using (var client = new HttpClient())
+
+
+
+           static FormTypeForm.RootObject getForms()
             {
 
-                client.DefaultRequestHeaders.TryAddWithoutValidation("authorization", "bearer " + token);
+                string url = "https://api.typeform.com/forms";
+                using (var client = new HttpClient())
+                {
 
-                var response = await client.GetAsync(url);
-                Task<string> responseString = response.Content.ReadAsStringAsync();
-                string outputJson = await responseString;
-                Console.WriteLine("finis");
-                var res = JsonConvert.DeserializeObject<RootObject>(outputJson);
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("authorization", "bearer " + token);
 
+                    string outputJson = getJson(url).Result;
 
+                    FormTypeForm.RootObject res = JsonConvert.DeserializeObject<FormTypeForm.RootObject>(outputJson);
 
-                return;
+                    return res;
+                }
+
             }
 
-        }
 
-
-
-        async static Task<RootObject> getForms()
-        {
-
-            string url = "https://api.typeform.com/forms";
-            using (var client = new HttpClient())
+            static FormTypeForm.RootObject getForm(string id_form)
             {
 
-                client.DefaultRequestHeaders.TryAddWithoutValidation("authorization", "bearer " + token);
-
-                var response = await client.GetAsync(url);
-                Task<string> responseString = response.Content.ReadAsStringAsync();
-                string outputJson = await responseString;
-
-                RootObject res = JsonConvert.DeserializeObject<RootObject>(outputJson);
-
-                return res;
-            }
-
-        }
-
-
-        async static Task<FormTypeForm.RootObject> getForm(string id_form)
-        {
-
-            string url = "https://api.typeform.com/forms/" + id_form;
-
-            using (var client = new HttpClient())
-            {
-
-                client.DefaultRequestHeaders.TryAddWithoutValidation("authorization", "bearer " + token);
-
-                var response = await client.GetAsync(url);
-                Task<string> responseString = response.Content.ReadAsStringAsync();
-                string outputJson = await responseString;
+                string url = "https://api.typeform.com/forms/" + id_form;
+                string outputJson = getJson(url).Result;
 
                 FormTypeForm.RootObject res = JsonConvert.DeserializeObject<FormTypeForm.RootObject>(outputJson);
 
@@ -129,149 +53,146 @@ namespace ConsoleApp1
             }
 
 
-
-        }
-
-
-        async static Task<AnswerTypeForm.RootObject> getAnswers(string id_form)
-        {
-
-            string url = "https://api.typeform.com/forms/" + id_form + "/responses";
-
-            using (var client = new HttpClient())
+            static AnswerTypeForm.RootObject getAnswers(string id_form)
             {
 
-                client.DefaultRequestHeaders.TryAddWithoutValidation("authorization", "bearer " + token);
+                string url = "https://api.typeform.com/forms/" + id_form + "/responses";
 
-                var response = await client.GetAsync(url);
-                Task<string> responseString = response.Content.ReadAsStringAsync();
-                string outputJson = await responseString;
 
+                string outputJson = getJson(url).Result;
                 AnswerTypeForm.RootObject res = JsonConvert.DeserializeObject<AnswerTypeForm.RootObject>(outputJson);
-
-
                 return res;
+                
             }
 
-
-
-        }
-
-
-
-        static void Main(string[] args)
-        {
-            //RootObject json_forms = getForms().Result;
-
-
-            FormTypeForm.RootObject json_form = getForm(id_form).Result;
-
-            Console.WriteLine("Nom du formulaire: " + json_form.title);
-
-            foreach (FormTypeForm.Field field in json_form.fields)
+            async static Task<string> getJson(string url)
             {
-                Console.WriteLine("Titre question: " + field.title);
-
-                if(field.properties != null && field.properties.fields != null)
+                using (var client = new HttpClient())
                 {
-                    foreach (FormTypeForm.Field2 sous_question in field.properties.fields)
-                    {
-                        Console.WriteLine("Question: " + sous_question.title);
-                    }
+
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("authorization", "bearer " + token);
+
+                    var response = await client.GetAsync(url);
+                    Task<string> responseString = response.Content.ReadAsStringAsync();
+                    string outputJson = await responseString;
+                    return outputJson;
                 }
-                
-                
             }
 
-            Console.WriteLine("");
-
-            AnswerTypeForm.RootObject json_answers = getAnswers(id_form).Result;
-            //Console.WriteLine(json_answers.items[0].answers[0].choice.label);
-            if (json_answers.items != null)
+            static void Main(string[] args)
             {
+                //RootObject json_forms = getForms().Result;
 
 
+                FormTypeForm.RootObject json_form = getForm(id_form);
 
-                foreach (AnswerTypeForm.Item field in json_answers.items)
+                Console.WriteLine("Nom du formulaire: " + json_form.title);
+
+                foreach (FormTypeForm.Field field in json_form.fields)
                 {
-                    if (field.answers != null)
+                    Console.WriteLine("Titre question: " + field.title);
+
+                    if (field.properties != null && field.properties.fields != null)
                     {
-
-
-                        foreach (AnswerTypeForm.Answer answer in field.answers)
+                        foreach (FormTypeForm.Field sous_question in field.properties.fields)
                         {
-
-                            if (answer != null)
-                            {
-                                if (answer.choice != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.choice.label);
-
-                                }
-
-                                else if (answer.choices != null)
-                                {
-                                    foreach (string choix in answer.choices.labels)
-                                    {
-                                        Console.WriteLine("Réponses: " + choix);
-
-                                    }
-                                }
-
-                                if (answer.text != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.text);
-                                }
-
-                                if (answer.url != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.url);
-                                }
-
-                                if (answer.boolean != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.boolean);
-                                }
-
-                                if (answer.number != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.number);
-                                }
-
-                                if (answer.date != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.date);
-                                }
-
-                                if (answer.email != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.email);
-                                }
-
-                                if (answer.file_url != null)
-                                {
-                                    Console.WriteLine("Réponse: " + answer.file_url);
-                                }
-
-                            }
-
+                            Console.WriteLine("Question: " + sous_question.title);
                         }
                     }
 
-                    
 
                 }
 
+                Console.WriteLine("");
+
+                AnswerTypeForm.RootObject json_answers = getAnswers(id_form);
+                //Console.WriteLine(json_answers.items[0].answers[0].choice.label);
+                if (json_answers.items != null)
+                {
+
+
+
+                    foreach (AnswerTypeForm.Item field in json_answers.items)
+                    {
+                        if (field.answers != null)
+                        {
+
+
+                            foreach (AnswerTypeForm.Answer answer in field.answers)
+                            {
+
+                                if (answer != null)
+                                {
+                                    if (answer.choice != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.choice.label);
+
+                                    }
+
+                                    else if (answer.choices != null)
+                                    {
+                                        foreach (string choix in answer.choices.labels)
+                                        {
+                                            Console.WriteLine("Réponses: " + choix);
+
+                                        }
+                                    }
+
+                                    if (answer.text != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.text);
+                                    }
+
+                                    if (answer.url != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.url);
+                                    }
+
+                                    if (answer.boolean != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.boolean);
+                                    }
+
+                                    if (answer.number != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.number);
+                                    }
+
+                                    if (answer.date != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.date);
+                                    }
+
+                                    if (answer.email != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.email);
+                                    }
+
+                                    if (answer.file_url != null)
+                                    {
+                                        Console.WriteLine("Réponse: " + answer.file_url);
+                                    }
+
+                                }
+
+                            }
+                        }
+
+
+
+                    }
+
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Pas de réponses");
+
+
+                }
                 Console.ReadKey();
-                return;
             }
-            else
-            {
-                Console.WriteLine("Pas de réponses");
-
-
-            }
-            Console.ReadKey();
         }
     }
 }
--- form_json.cs
+++ form_json.cs
@@ -6,15 +6,9 @@ using System.Threading.Tasks;
 
 namespace FormTypeForm
 {
-    public class Theme
-    {
-        public string href { get; set; }
-    }
+    
 
-    public class Workspace
-    {
-        public string href { get; set; }
-    }
+    
 
     public class Image
     {
@@ -27,23 +21,9 @@ namespace FormTypeForm
         public Image image { get; set; }
     }
 
-    public class Settings
-    {
-        public bool is_public { get; set; }
-        public bool is_trial { get; set; }
-        public string language { get; set; }
-        public string progress_bar { get; set; }
-        public bool show_progress_bar { get; set; }
-        public bool show_typeform_branding { get; set; }
-        public Meta meta { get; set; }
-    }
+    
 
-    public class Properties
-    {
-        public bool show_button { get; set; }
-        public string description { get; set; }
-        public string button_text { get; set; }
-    }
+    
 
     public class Attachment
     {
@@ -51,55 +31,14 @@ namespace FormTypeForm
         public string href { get; set; }
     }
 
-    public class WelcomeScreen
-    {
-        public string @ref { get; set; }
-        public string title { get; set; }
-        public Properties properties { get; set; }
-        public Attachment attachment { get; set; }
-    }
-
-    public class Properties2
-    {
-        public bool show_button { get; set; }
-        public bool share_icons { get; set; }
-        public string button_mode { get; set; }
-        public string button_text { get; set; }
-        public string redirect_url { get; set; }
-    }
-
-    public class Attachment2
-    {
-        public string type { get; set; }
-        public string href { get; set; }
-    }
-
-    public class ThankyouScreen
-    {
-        public string @ref { get; set; }
-        public string title { get; set; }
-        public Properties2 properties { get; set; }
-        public Attachment2 attachment { get; set; }
-    }
-
-    public class Validations
-    {
-        public bool required { get; set; }
-        public int max_length { get; set; }
-    }
-
-    public class Attachment3
-    {
-        public string type { get; set; }
-        public string href { get; set; }
-    }
+    
 
     public class Choice
     {
         public string id { get; set; }
         public string @ref { get; set; }
         public string label { get; set; }
-        public Attachment3 attachment { get; set; }
+        public Attachment attachment { get; set; }
     }
 
     public class Labels
@@ -108,7 +47,7 @@ namespace FormTypeForm
         public string right { get; set; }
     }
 
-    public class Properties4
+    public class Properties
     {
         public string description { get; set; }
         public bool? randomize { get; set; }
@@ -116,6 +55,7 @@ namespace FormTypeForm
         public bool? allow_other_choice { get; set; }
         public bool? vertical_alignment { get; set; }
         public List<Choice> choices { get; set; }
+        public List<Field> fields { get; set; }
         public bool? supersized { get; set; }
         public bool? show_labels { get; set; }
         public bool? hide_marks { get; set; }
@@ -128,114 +68,24 @@ namespace FormTypeForm
         public string shape { get; set; }
     }
 
-    public class Attachment4
-    {
-        public string type { get; set; }
-        public string href { get; set; }
-    }
-
-    public class Field2
-    {
-        public string id { get; set; }
-        public string title { get; set; }
-        public string @ref { get; set; }
-        public Validations validations { get; set; }
-        public string type { get; set; }
-        public Properties4 properties { get; set; }
-        public Attachment4 attachment { get; set; }
-    }
-
-    public class Choice2
-    {
-        public string id { get; set; }
-        public string @ref { get; set; }
-        public string label { get; set; }
-    }
-
-    public class Properties3
-    {
-        public bool show_button { get; set; }
-        public string button_text { get; set; }
-        public List<Field2> fields { get; set; }
-        public bool? hide_marks { get; set; }
-        public bool? randomize { get; set; }
-        public bool? allow_multiple_selection { get; set; }
-        public bool? allow_other_choice { get; set; }
-        public bool? vertical_alignment { get; set; }
-        public List<Choice2> choices { get; set; }
-        public string structure { get; set; }
-        public string separator { get; set; }
-    }
-
-    public class Attachment5
-    {
-        public string type { get; set; }
-        public string href { get; set; }
-    }
-
-    public class Validations2
-    {
-        public bool required { get; set; }
-    }
-
+   
     public class Field
     {
         public string id { get; set; }
         public string title { get; set; }
         public string @ref { get; set; }
-        public Properties3 properties { get; set; }
-        public Attachment5 attachment { get; set; }
+        public Properties properties { get; set; }
+        public Attachment attachment { get; set; }
         public string type { get; set; }
-        public Validations2 validations { get; set; }
     }
 
-    public class To
-    {
-        public string type { get; set; }
-        public string value { get; set; }
-    }
-
-    public class Details
-    {
-        public To to { get; set; }
-    }
-
-    public class Condition
-    {
-        public string op { get; set; }
-        public List<object> vars { get; set; }
-    }
-
-    public class Action
-    {
-        public string action { get; set; }
-        public Details details { get; set; }
-        public Condition condition { get; set; }
-    }
-
-    public class Logic
-    {
-        public string type { get; set; }
-        public string @ref { get; set; }
-        public List<Action> actions { get; set; }
-    }
-
-    public class Links
-    {
-        public string display { get; set; }
-    }
+    
 
     public class RootObject
     {
         public string id { get; set; }
         public string title { get; set; }
-        public Theme theme { get; set; }
-        public Workspace workspace { get; set; }
-        public Settings settings { get; set; }
-        public List<WelcomeScreen> welcome_screens { get; set; }
-        public List<ThankyouScreen> thankyou_screens { get; set; }
         public List<Field> fields { get; set; }
-        public List<Logic> logic { get; set; }
-        public Links _links { get; set; }
+        
     }
 }
